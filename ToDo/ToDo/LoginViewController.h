30d143c9811e423cebebe57515459aadb4351c45
Nebojsa Gujanicic <nebojasa@gmail.com>
2016-03-30 15:07:32 +0200

    LoginViewController added to project.

---
+++ ToDo/ToDo/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ToDo
+//
+//  Created by Cubes School 10 on 3/30/16.
+//  Copyright © 2016 Cubes School 10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ ToDo/ToDo/LoginViewController.m
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.m
+//  ToDo
+//
+//  Created by Cubes School 10 on 3/30/16.
+//  Copyright © 2016 Cubes School 10. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+@end
