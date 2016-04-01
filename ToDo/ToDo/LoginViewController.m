140754f8ad45f4fa686ea445ad066a5e80673f8c
Nebojsa Gujanicic <nebojasa@gmail.com>
2016-04-01 15:55:03 +0200

    Login view controller almost completed...

--- ToDo/ToDo/LoginViewController.m
+++ ToDo/ToDo/LoginViewController.m
@@ -8,5 +8,31 @@
 
 #import "LoginViewController.h"
 
-@implementation LoginViewController
+@interface LoginViewController()
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @end
+
+@implementation LoginViewController
+
+#pragma mark - Private API
+
+- (void)configureTextField:(UITextField *)textField {
+    if (textField.placeholder.length > 0) {
+//        UIColor *color = [UIColor colorWithRed:117.0/255.0
+//                                         green:113.0/255.0
+//                                          blue:111.0/255.0
+//                                         alpha:1.0];
+        NSDictionary *attributes = @{
+                                    NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Regular" size:14.0], NSForegroundColorAttributeName: [UIColor whiteColor]
+                                    };
+    textField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:textField.placeholder attributes:attributes];
+    
+    }
+}
+
+
+@end
+
